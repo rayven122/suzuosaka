b4f17a5e88b6215a8e3750052f353d4a1a72f3a5
yoshiy-dev <toplsm9674@gmail.com>
2025-02-06 12:44:19 +0900

    Update Hero component layout and OGP image

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -87,9 +87,9 @@ export const Hero = () => {
       {/* 下部 */}
       <div className="absolute bottom-0 left-0 w-full">
         {/* お品書き */}
-        <ul className="mb-6 ml-3 flex min-w-[136px] space-x-2 bg-yellow-500/60 font-bold md:mb-10 md:ml-14 md:space-x-4 md:text-3xl lg:text-4xl">
+        <ul className="mb-6 ml-3 flex bg-yellow-500/60 font-bold md:mb-10 md:ml-14 md:text-3xl lg:text-4xl">
           {["お食事", "釣り", "養殖場見学", "体験"].map((text, index) => (
-            <li key={index} className="block">
+            <li key={index} className="mr-2 block md:mr-4">
               <span className="border-2 border-black bg-primary [writing-mode:vertical-rl]">
                 {text}
               </span>
